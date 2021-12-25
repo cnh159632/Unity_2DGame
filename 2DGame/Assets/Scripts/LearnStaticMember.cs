de7043dd85b2cae60f6c5992c394e4064788a2bb
cnh159632 <cnh159632@gmail.com>
2021-12-25 15:31:09 +0800

    練習C#

---
+++ 2DGame/Assets/Scripts/LearnStaticMember.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class LearnStaticMember : MonoBehaviour
+{
+    private void Start()
+    {
+        //存放靜態屬性
+        Cursor.visible = false; //指標.可視 = 否;
+
+        //取得靜態屬性
+        print(Mathf.PI);        //數學.圓周率
+        print(Random.value);    //隨機.值 - 0 ~ 1 之間
+
+        //使用靜態方法
+        print(Mathf.Abs(-100));             //數學.絕對值(數值)
+        print(Random.Range(100, 2000));     //隨機.範圍(最小值,最大值)
+    }
+
+    private void Update()
+    {
+        print(Input.GetKeyDown("space"));   //輸入.按下按鍵("按鍵名稱")
+    }
+}
